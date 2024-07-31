70e33012c50d95d5f5df042b1c952a2075ebc815
kartik sharma <kartiksharma88103@gmail.com>
2024-07-31 10:17:39 +0530

    completed intro text part

--- src/Components/Front.jsx
+++ src/Components/Front.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import './Front.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faXTwitter ,faLinkedinIn ,  faGithub ,faInstagram} from '@fortawesome/free-brands-svg-icons'
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
+
 
 export const Front = () => {
   return (
-    <>
-    Kartik Sharma
-    </>
+    <div className="body">
+      <div className="intro mt-5">
+        <div className="intro_text">
+          <h3 className='text-light ms-2' >I am Kartik sharma</h3>
+          <h1>Java Full Stack <br/> Developer</h1>
+          <h5 className='mt-3'>
+            Crafting seamless web experiences from front to back with Java expertise! Passionate about building
+            innovative and scalable solutions
+          </h5>
+
+          <div className="intro_buttons mt-4">
+            <button className='bt'>Download CV <FontAwesomeIcon  icon={faDownload} /></button>
+            <FontAwesomeIcon className='icons' icon={faXTwitter} />
+            <FontAwesomeIcon className='icons' icon={faLinkedinIn} />
+            <FontAwesomeIcon className='icons' icon={faGithub} />
+            <FontAwesomeIcon className='icons' icon={faInstagram} />
+          </div>
+        </div>
+      </div>
+
+      <div className="intro_image">
+
+      </div>
+    </div>
   )
 }
